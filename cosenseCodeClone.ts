a488cf5e73c5777a3e0deeac3275d6604fd2c5bc
bsahd <193844131+bsahd@users.noreply.github.com>
2025-03-12 17:32:34 +0900

    HTML生成機能を削除、高速化

--- cosenseCodeClone.ts
+++ cosenseCodeClone.ts
@@ -1,151 +1,9 @@
-import * as parser from "@progfay/scrapbox-parser"; // MIT License
+import * as parser from "@progfay/scrapbox-parser"
 import type * as cosenseTypes from "@cosense/types/rest"; // MIT License
-import * as cosenseStd from "@cosense/std"; // MIT License
+import * as libcosense from "@bsahd/libcosense"; // MIT License
 import ProgressBar from "@deno-library/progress"; // MIT License
 import { delay } from "@std/async"; // MIT License
 
-function hsc(unsafeText: string) {
-  if (typeof unsafeText !== "string") {
-    return unsafeText;
-  }
-  return unsafeText.replace(
-    /[&'`"<>]/g,
-    function (match) {
-      return {
-        "&": "&amp;",
-        "'": "&#x27;",
-        "`": "&#x60;",
-        '"': "&quot;",
-        "<": "&lt;",
-        ">": "&gt;",
-      }[match as "&" | "'" | "`" | '"' | "<" | ">"];
-    },
-  );
-}
-function arrayRange(start: number, stop: number, step: number) {
-  return Array.from(
-    { length: (stop - start) / step + 1 },
-    (_, index) => start + index * step,
-  );
-}
-
-function genereteHTML_Node(node: parser.Node, slashcount: number): string {
-  switch (node.type) {
-    case "quote":
-      return `<q>${
-        node.nodes.map((a) => genereteHTML_Node(a, slashcount)).join("")
-      }</q>`;
-    case "helpfeel":
-      return node.text;
-    case "strong":
-      return `<strong>${
-        node.nodes.map((a) => genereteHTML_Node(a, slashcount)).join("")
-      }</strong>`;
-    case "formula":
-      return `<code>math:${hsc(node.formula)}</code>`;
-    case "decoration":
-      return node.nodes.map((a) => genereteHTML_Node(a, slashcount))
-        .join("");
-    case "code":
-      return `<code>${hsc(node.text)}</code>`;
-    case "commandLine":
-      return `<code>${hsc(node.text)}</code>`;
-    case "blank":
-      return "";
-    case "strongImage":
-    case "image":
-      return `<a href="${hsc(encodeURI(node.src))}">[image]</a>`;
-    case "link":
-      if (node.pathType == "absolute") {
-        return `<a href="${hsc(encodeURI(node.href))}">${
-          hsc(node.content)
-        }</a>`;
-      } else if (node.pathType == "root") {
-        return `<a href="https://scrapbox.io${hsc(node.href)}">${
-          hsc(node.href)
-        }</a>`;
-      }
-      return `<a href="${"../".repeat(slashcount)}${
-        hsc(sanitizeDirName(node.href))
-      }/text.html">${hsc(node.href)}</a>`;
-    case "googleMap":
-      return `<a href="${hsc(encodeURI(node.url))}">[map]</a>`;
-    case "strongIcon":
-    case "icon":
-      if (node.pathType == "root") {
-        return `<a href="https://scrapbox.io${hsc(node.path)}">[${
-          hsc(node.path)
-        }.icon]</a>`;
-      }
-      return `<a href="${"../".repeat(slashcount)}${
-        hsc(sanitizeDirName(node.path))
-      }/text.html">[${hsc(node.path)}.icon]</a>`;
-    case "hashTag":
-      return `<a href="${"../".repeat(slashcount)}${
-        hsc(sanitizeDirName(node.href))
-      }/text.html">#${hsc(node.href)}</a>`;
-    case "numberList":
-      return `${node.number}. ${
-        node.nodes.map((a) => genereteHTML_Node(a, slashcount))
-          .join("")
-      }`;
-    case "plain":
-      return node.text;
-  }
-}
-function generateHTML(
-  page: parser.Page,
-  slashcount: number,
-  related: cosenseTypes.RelatedPages,
-) {
-  let html = `
-	<!DOCTYPE HTML><html><head><title>${
-    page[0].type == "title" ? page[0].text : "unknown"
-  }</title></head><body>
-	<a href="${
-    "../".repeat(slashcount)
-  }index.html">top</a>, <a href="https://scrapbox.io/${PROJECT_NAME}/${
-    page[0].type == "title" ? page[0].text : ""
-  }">online</a>, <a href="./">source</a>`;
-  for (const block of page) {
-    if (block.type == "title") {
-      html += `<h1>${hsc(block.text)}</h1>`;
-    } else if (block.type == "line") {
-      html += `<p style="margin-left:${block.indent}em;">${
-        block.nodes.map((a) => genereteHTML_Node(a, slashcount)).join(
-          "",
-        )
-      }</p>`;
-    } else if (block.type == "table") {
-      html += `<table style="margin-left:${block.indent}em;">`;
-      html += `table:${hsc(block.fileName)}`;
-      for (const rows of block.cells) {
-        html += `<tr>`;
-        for (const cols of rows) {
-          html += `<td>`;
-          html += cols.map((a) => genereteHTML_Node(a, slashcount))
-            .join("");
-          html += `</td>`;
-        }
-        html += `</tr>`;
-      }
-      html += `</table>`;
-    } else if (block.type == "codeBlock") {
-      html += `<p><a href="${hsc(sanitizeFileName(block.fileName))}">code:${
-        hsc(block.fileName)
-      }</a></p>`;
-    }
-  }
-  html += `<h1>1 hop links</h1><ul>`;
-  for (const element of related.links1hop) {
-    html += `<li><a href="${"../".repeat(slashcount)}${
-      hsc(sanitizeDirName(element.titleLc))
-    }/text.html">${hsc(element.titleLc)}</a></li>`;
-  }
-  html += `</ul>`;
-  html += `</body></html>`;
-  return html;
-}
 
 function sanitizeDirName(name: string) {
   // Windowsで使えない文字を考慮
@@ -177,45 +35,9 @@ function sanitizeFileName(name: string) {
     return name.replaceAll("/", "%2F");
   }
 }
-async function generateIndexHTML(indexPages: string[]) {
-  indexPages.sort();
-  const indexGrouped = Object.groupBy(indexPages, (a) => a[0]);
-  await Deno.writeTextFile(
-    `./${destination}/index.html`,
-    `
-	<!DOCTYPE HTML>
-	<html>
-		<head>
-			<title>pages of ${hsc(PROJECT_NAME)}</title>
-		</head>
-		<body>
-			<h1>pages of ${hsc(PROJECT_NAME)}</h1>
-			${
-      Object.entries(indexGrouped).map(([k, _]) =>
-        `<a href="#${hsc(k)}" style="padding-inline-end:4px;">${hsc(k)}</a>`
-      ).join("")
-    }
-			<ul>
-			${
-      Object.entries(indexGrouped).map(([k, v]) =>
-        `<li id="${hsc(k)}">${hsc(k)}<ul>${
-          v?.map((v) =>
-            `<li><a href="${hsc(encodeURI(sanitizeDirName(v)))}/text.html">${
-              hsc(v)
-            }</a></li>`
-          ).join("")
-        }</ul></li>`
-      ).join("")
-    }
-			</ul>
-		</body>
-	</html>
-	`,
-  );
-}
 
-async function getPage(pagei: cosenseTypes.BasePage, MODE: "clone" | "pull") {
-  const pagename = pagei.title;
+async function getPage(page: libcosense.Page, MODE: "clone" | "pull") {
+  const pagename = page.title;
   try {
     if(pagename.includes("../")){
       console.log("pagename \""+pagename+"\" is danger!not clone this page.")
@@ -230,7 +52,7 @@ async function getPage(pagei: cosenseTypes.BasePage, MODE: "clone" | "pull") {
         )).mtime?.getTime();
         if (
           filestat &&
-          filestat / 1000 > pagei.updated
+          filestat / 1000 > page.updated
         ) {
           return;
         } else {
@@ -253,11 +75,6 @@ async function getPage(pagei: cosenseTypes.BasePage, MODE: "clone" | "pull") {
       `./${destination}/${sanitizeDirName(pagename)}`,
       { recursive: true },
     );
-    const page: cosenseTypes.Page = await (await fetch(
-      `https://scrapbox.io/api/pages/${PROJECT_NAME}/${
-        encodeURIComponent(pagename)
-      }`,
-    )).json();
     const pagetext = page.lines.map((a) => a.text).join("\n");
     const pageparse = parser.parse(
       pagetext,
@@ -265,14 +82,6 @@ async function getPage(pagei: cosenseTypes.BasePage, MODE: "clone" | "pull") {
     await Deno.writeTextFile(
       `./${destination}/${sanitizeDirName(pagename)}/text.txt`,
       pagetext,
-    );
-    await Deno.writeTextFile(
-      `./${destination}/${sanitizeDirName(pagename)}/text.html`,
-      generateHTML(
-        pageparse,
-        pagename.split("/").length,
-        page.relatedPages,
-      ),
     );
     await Deno.writeTextFile(
       `./${destination}/${sanitizeDirName(pagename)}/json.json`,
@@ -339,31 +148,18 @@ export async function cloneFromAPI(
   const PARALLEL = 16;
   
   const pagelist: cosenseTypes.BasePage[] = [];
-  let progress = new ProgressBar({ title: "fetching page list:", total: pageCount });
-  for (const skipnum of arrayRange(0, pageCount, 1000)) {
-    const pglistw = await cosenseStd.listPages(PROJECT_NAME, {
-      skip: skipnum,
-      limit: 1000,
-      sort: "updated",
-    });
-    if (!pglistw.ok) {
-      continue;
-    }
-    progress.render(skipnum)
-    pagelist.push(...pglistw.val.pages);
-  }
+  const pj = await libcosense.Project.new(PROJECT_NAME)
   indexPages.push(...pagelist.map((a) => a.title));
-  progress = new ProgressBar({ title: "Cloning:", total: pageCount });
-  await generateIndexHTML(indexPages);
+  const progress = new ProgressBar({ title: "Cloning:", total: pageCount });
   let connections = 0;
   let getted = 0;
-  for (const item of pagelist) {
+  for await (const item of pj.pageList()) {
     while (connections > PARALLEL) {
       await delay(50);
     }
     await progress.render(getted);
     connections++;
-    getPage(item, MODE).then(() => {
+    getPage(await item.getDetail(), MODE).then(() => {
       getted++;
       connections--;
     });
